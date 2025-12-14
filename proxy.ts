dfc3cd5639694e5f0dc29881e52320bf5306cbf8
Andrew Zhao <andrewzhao5620@gmail.com>
2025-12-14 15:46:34 -0500

    Updating proxy.ts file with updated API

--- proxy.ts
+++ proxy.ts
@@ -1,18 +1,14 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { withClerkMiddleware } from "@clerk/nextjs";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)"]);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
-  }
-});
+export default withClerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and static files
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
+    // Exclude sign-in route
+    "!/sign-in(.*)",
   ],
 };
