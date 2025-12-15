f6636e71f8045606a589e61a883aa85d391588ff
Andrew Zhao <andrewzhao5620@gmail.com>
2025-12-14 20:16:18 -0500

    Hoping to fix favorite not working in production but only locally

--- convex/boards.ts
+++ convex/boards.ts
@@ -11,15 +11,10 @@ export const get = query({
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
 
-    // Unauthorized check
     if (!identity) {
       throw new Error("Unauthorized");
     }
 
-    console.log("Convex userId:", identity.subject);
-    console.log("Convex orgId:", args.orgId);
-
-    // If fetching only favorites
     if (args.favorites) {
       const favoritedBoards = await ctx.db
         .query("userFavorites")
@@ -29,12 +24,8 @@ export const get = query({
         .order("desc")
         .collect();
 
-      console.log(
-        "Favorited boards for user in org:",
-        favoritedBoards.map((b) => b.boardId)
-      );
-
       const ids = favoritedBoards.map((b) => b.boardId);
+
       const boards = await getAllOrThrow(ctx.db, ids);
 
       return boards.map((board) => ({
@@ -43,13 +34,14 @@ export const get = query({
       }));
     }
 
-    // Otherwise, fetch all boards (optionally filtered by search)
-    let boards: any[] = [];
-    if (args.search) {
+    const title = args.search as string;
+    let boards = [];
+
+    if (title) {
       boards = await ctx.db
         .query("boards")
         .withSearchIndex("search_title", (q) =>
-          q.search("title", args.search!).eq("orgId", args.orgId)
+          q.search("title", title).eq("orgId", args.orgId)
         )
         .collect();
     } else {
@@ -59,20 +51,23 @@ export const get = query({
         .collect();
     }
 
-    // Attach isFavorite property
-    const boardsWithFavoriteRelation = boards.map(async (board) => {
-      const favorite = await ctx.db
+    const boardsWithFavoriteRelation = boards.map((board) => {
+      return ctx.db
         .query("userFavorites")
         .withIndex("by_user_board", (q) =>
           q.eq("userId", identity.subject).eq("boardId", board._id)
         )
-        .unique();
-      return {
-        ...board,
-        isFavorite: !!favorite,
-      };
+        .unique()
+        .then((favorite) => {
+          return {
+            ...board,
+            isFavorite: !!favorite,
+          };
+        });
     });
 
-    return Promise.all(boardsWithFavoriteRelation);
+    const boardsWithFavoriteBoolean = Promise.all(boardsWithFavoriteRelation);
+
+    return boardsWithFavoriteBoolean;
   },
 });
